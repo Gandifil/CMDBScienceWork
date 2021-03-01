00a4b8086bbbb23999348481b05245d15b7e63a4
Max Kopjev <GandifilMK@gmail.com>
2021-03-01 19:42:07 +0300

    Fix modal form for creating equipment up.

--- Console/ClientApp/src/components/Modals/CreateEquipment.js
+++ Console/ClientApp/src/components/Modals/CreateEquipment.js
@@ -1,10 +1,24 @@
 ﻿import React, { Component } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Label, Input, FormGroup } from 'reactstrap';
 
 export class CreateEquipment extends Component {
     constructor(props) {
         super(props);
-        this.state = {visible: false}
+        this.state = {
+            item: { name: "", cost: 0 },
+            visible: false
+        }
+
+        this.handleName = (e) => this.setName(e.target.value)
+        this.handleCost = (e) => this.setCost(e.target.value)
+    }
+
+    setName(value) {
+        this.setState({ item: { name: value, cost: this.state.item.cost }})
+    }
+
+    setCost(value) {
+        this.setState({ item: {name: this.state.item.name, cost: value } })
     }
 
     show() {
@@ -19,21 +33,30 @@ export class CreateEquipment extends Component {
         this.setState({ visible: !this.state.visible })
     }
 
+    async submit() {
+        console.log("Creating equipment", this.state.item)
+        this.hide();
+    }
+
     render() {
         return (
-                <Modal isOpen={this.state.visible} >
-                    <ModalHeader>Modal title</ModalHeader>
-                    <ModalBody>
-                    I will not close if you click outside me. Don't even try to press
-                    escape key.
-        </ModalBody>
-                    <ModalFooter>
-                    <Button variant="secondary" onClick={() => this.hide()}>
-                        Close
-          </Button>
-                        <Button variant="primary">Understood</Button>
-                    </ModalFooter>
-                </Modal>
+<Modal isOpen={this.state.visible} >
+    <ModalHeader>Добавление оборудования</ModalHeader>
+    <ModalBody>
+        <FormGroup>
+                        <Label>Имя</Label>
+                        <Input name="name" placeholder="0001abcd02" onChange={this.handleName} />
+        </FormGroup>
+        <FormGroup>
+            <Label>Цена</Label>
+                        <Input type="number" name="cost" placeholder="1000" onChange={this.handleCost} />
+        </FormGroup>
+    </ModalBody>
+    <ModalFooter>
+    <Button variant="secondary" onClick={() => this.hide()}>Назад</Button>
+                    <Button variant="primary" onClick={() => this.submit()}>Создать</Button>
+    </ModalFooter>
+</Modal>
         );
     }
 }
