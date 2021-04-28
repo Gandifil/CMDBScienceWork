be14367fda25b79348ca561d18c912a3af40633d
Gandifil <Gandifilmk@gmail.com>
2021-04-28 03:23:43 +0300

    Refactor add button to extern function.

--- Console/ClientApp/src/components/Attributes/AttributesPage.js
+++ Console/ClientApp/src/components/Attributes/AttributesPage.js
@@ -3,6 +3,7 @@ import ReactLoading from 'react-loading';
 import { Button } from 'reactstrap';
 import { AttributesTable } from './AttributesTable';
 import { EditAttributeModal } from './EditAttributeModal';
+import { AddButton } from '../AddButton';
 
 export class AttributesPage extends Component {
     constructor(props) {
@@ -25,18 +26,9 @@ export class AttributesPage extends Component {
             ? <ReactLoading type="cylon" color="black" height={667} width={375} />
             : <AttributesTable items={this.state.items} />;
 
-               // <Button color="primary" size="lg" block onClick={this.handleClick}>Фильтрация</Button>
-               // <ArticleSearchModal ref={this.modal} onSubmit={ this.handleModalSubmit } />
         return (
             <div>
-                <Button className="m-3" size="lg" color="primary" onClick={this.handleCreateClick}>
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-node-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M0 7.5A1.5 1.5 0 0 1 1.5 6h1A1.5 1.5 0 0 1 4 7.5v1A1.5 1.5 0 0 1 2.5 10h-1A1.5 1.5 0 0 1 0 8.5v-1zM1.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM7 8.5H4v-1h3v1z" />
-                        <path fill-rule="evenodd" d="M11 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0 1a5 5 0 1 0 0-10 5 5 0 0 0 0 10z" />
-                        <path fill-rule="evenodd" d="M11 5a.5.5 0 0 1 .5.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2A.5.5 0 0 1 11 5z" />
-                    </svg>
-                    Добавить атрибут
-                </Button>
+                <AddButton name="Добавить атрибут" onClick={this.handleCreateClick}/>
                 <EditAttributeModal ref={this.createModal} onAccept={this.handleCreateAccept} />
                 {contents}
             </div>
--- Console/ClientApp/src/components/Metrics/MetricsPage.js
+++ Console/ClientApp/src/components/Metrics/MetricsPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactLoading from 'react-loading';
 import { Button } from 'reactstrap';
 import { MetricsTable } from './MetricsTable';
+import { AddButton } from '../AddButton';
 //import { EditMetricModal } from './EditMetricModal';
 
 export class MetricsPage extends Component {
@@ -27,15 +28,7 @@ export class MetricsPage extends Component {
 
         return (
             <div>
-                <Button className="m-3" size="lg" color="primary" onClick={this.handleCreateClick}>
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-node-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M0 7.5A1.5 1.5 0 0 1 1.5 6h1A1.5 1.5 0 0 1 4 7.5v1A1.5 1.5 0 0 1 2.5 10h-1A1.5 1.5 0 0 1 0 8.5v-1zM1.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM7 8.5H4v-1h3v1z" />
-                        <path fill-rule="evenodd" d="M11 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0 1a5 5 0 1 0 0-10 5 5 0 0 0 0 10z" />
-                        <path fill-rule="evenodd" d="M11 5a.5.5 0 0 1 .5.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2A.5.5 0 0 1 11 5z" />
-                    </svg>
-                    Добавить метрику
-                </Button>
-                
+                <AddButton name="Добавить метрику" onClick={this.handleCreateClick}/>
                 {contents}
             </div>
             );
