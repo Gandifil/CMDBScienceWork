4b280d412aac33700ac9f7cb93ab79622d354dd3
Gandifil <Gandifilmk@gmail.com>
2021-05-05 20:31:20 +0300

    Add react-async and EntitiesTable template.

--- Console/ClientApp/src/components/Elements/Equipments/EquipmentAttributesTab.js
+++ Console/ClientApp/src/components/Elements/Equipments/EquipmentAttributesTab.js
@@ -1,7 +1,17 @@
-import React, { Component } from 'react';
+﻿import React, { Component } from 'react';
+import { EntitiesTable } from '../../EntitiesTable';
 
 export function EquipmentAttributesTab(props) {
+    const typeNames = ["Строка", "Проценты", "Вещественное", "Целое"]
+
+    const renderItem = (item) =>
+        <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>{typeNames[item.type]}</td>
+            <td>{item.value}</td>
+        </tr>
+
     return (
-        <div>{"EquipmentAttributesTab"}</div>
+        <EntitiesTable title="Атрибуты" headers={["Имя", "Тип", "Значение"]} renderItem={renderItem} />
     );
 }
---
+++ Console/ClientApp/src/components/EntitiesTable.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import { Button, Container, ModalHeader, Table } from 'reactstrap';
+import { useAsync, useFetch } from "react-async"
+import ReactLoading from 'react-loading';
+import { AddButton } from './AddButton';
+import { ItemsTable } from './ItemsTable';
+
+function LoadingData(props) {
+    const { data, error, isPending } = useFetch(props.resource, {
+        headers: { accept: "application/json" },
+    })
+    if (isPending) return <ReactLoading type="cylon" color="black" className="m-auto" height={'30%'} width={'30%'} />
+    if (error) return `Something went wrong: ${error.message}`
+    if (data)
+        return props.render(data)
+    return null
+}
+
+export function EntitiesTable(props) {
+    const renderTable = (data) => 
+        <ItemsTable headers={props.headers} items={data} render={ props.renderItem } />
+
+    return (
+        <Container>
+            <AddButton name="Добавить" onClick={props.onAddClick} />
+            <LoadingData resource="/api/equipments/1/attributes" render={renderTable}/>
+        </Container>
+    );
+}
